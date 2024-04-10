b9f2b4e83bf2f4a88cf0ef9c84499269959e83a5
v012345 <v138468@gmail.com>
2024-04-10 17:22:37 +0800

    第一阶段链接

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+// https://learn.microsoft.com/en-us/training/paths/get-started-c-sharp-part-1/
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.Write("This is ");
