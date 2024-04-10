983ec21a12442adf29a3cb451d2ad364299b5ca1
v012345 <v138468@gmail.com>
2024-04-10 17:18:52 +0800

    控制台输出测试

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.Write("This is ");
+Console.Write("the second ");
+Console.Write("line.");
